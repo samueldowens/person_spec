501a161cda1ce1d5226697fdfaa25f7432280e60
samueldowens <samueldowens@gmail.com>
2013-10-10 09:17:43 -0400

    better solution

--- person.rb
+++ person.rb
@@ -4,34 +4,8 @@ class Person
 
   def initialize(hash)
     hash.each do |key, value|
-      case key
-        when :name
-          @name = value
-        when :birthday
-          @birthday = value
-        when :hair_color
-          @hair_color = value
-        when :eye_color
-          @eye_color = value
-        when :height
-          @height = value
-        when :weight
-          @weight = value
-        when :handed
-          @handed = value
-        when :complexion
-          @complexion = value
-        when :t_shirt_size
-          @t_shirt_size = value
-        when :wrist_size
-          @wrist_size = value
-        when :glove_size
-          @glove_size = value
-        when :pant_length
-          @pant_length = value
-        when :pant_width
-          @pant_width = value
-      end
+      self.send("#{key}=", value)
     end
   end
+
 end
